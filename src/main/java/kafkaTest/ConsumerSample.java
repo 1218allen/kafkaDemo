baf6d30f61d79d58ab3bf8e1ad8e2699dec78f91
gaolei <flynow2004@126.com>
2017-08-24 13:50:31 +0800

    fix bug

--- src/main/java/kafkaTest/ConsumerSample.java
+++ src/main/java/kafkaTest/ConsumerSample.java
@@ -1,44 +1,96 @@
 package kafkaTest;
 
-import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Properties;
+import java.util.concurrent.Executors;
 
-import org.apache.kafka.clients.consumer.ConsumerRecord;
-import org.apache.kafka.clients.consumer.ConsumerRecords;
-import org.apache.kafka.clients.consumer.KafkaConsumer;
-import org.apache.kafka.common.TopicPartition;
+import kafka.consumer.Consumer;
+import kafka.consumer.ConsumerConfig;
+import kafka.consumer.ConsumerIterator;
+import kafka.consumer.KafkaStream;
 
 public class ConsumerSample {
-	public static void main(String[] args) {
+	private kafka.javaapi.consumer.ConsumerConnector consumer;
+	private String topic;
+
+	public static void main(String[] arg) {
+		new ConsumerSample().start();
+	}
+
+	public void init() {
+		// 指定 zookeeper 的地址
+		String zookeeper = "localhost:2181";
+		String topic = "test";
+		String groupId = "test-group";
+
 		Properties props = new Properties();
-		props.put("bootstrap.servers", "localhost:9092");
-		props.put("group.id", "test");
-		props.put("enable.auto.commit", "true");
+		/**
+		 * 必须的配置
+		 */
+		props.put("zookeeper.connect", zookeeper);
+		/**
+		 * 必须的配置， 代表该消费者所属的 consumer group
+		 */
+		props.put("group.id", groupId);
+		/**
+		 * 多长时间没有发送心跳信息到zookeeper就会认为其挂掉了，默认是6000
+		 */
+		props.put("zookeeper.session.timeout.ms", "6000");
+		/**
+		 * 可以允许zookeeper follower 比 leader慢的时长
+		 */
+		props.put("zookeeper.sync.time.ms", "200");
+		/**
+		 * 控制consumer offsets提交到zookeeper的频率， 默认是60 * 1000
+		 */
 		props.put("auto.commit.interval.ms", "1000");
-		props.put("session.timeout.ms", "30000");
-		props.put("key.deserializer",
-				"org.apache.kafka.common.serialization.StringDeserializer");
-		props.put("value.deserializer",
-				"org.apache.kafka.common.serialization.StringDeserializer");
-		KafkaConsumer<String, String> consumer = new KafkaConsumer<String, String>(
-				props);
-		consumer.subscribe(Arrays.asList("foo", "bar", "my-topic"));
-//		while (true) {
-//			ConsumerRecords<String, String> records = consumer.poll(100);
-//			for (ConsumerRecord<String, String> record : records)
-//				System.out.printf("offset = %d, key = %s, value = %s",
-//						record.offset(), record.key(), record.value());
-//		}
-		
-		while(true){
-			ConsumerRecords<String, String> res = consumer.poll(100);
-			for(TopicPartition p : res.partitions()){
-				List<ConsumerRecord<String, String>> pRes = res.records(p);
-				for(ConsumerRecord<String, String>r:pRes){
-					r.value();
-				}
+
+		consumer = Consumer.createJavaConsumerConnector(new ConsumerConfig(props));
+		this.topic = topic;
+	}
+
+	public void consume() {
+		Map<String, Integer> topicCountMap = new HashMap<String, Integer>();
+		topicCountMap.put(topic, 1);
+		/**
+		 * createMessageStreams 为每个topic创建 message stream
+		 */
+		Map<String, List<KafkaStream<byte[], byte[]>>> messageStreams = consumer.createMessageStreams(topicCountMap);
+		KafkaStream<byte[], byte[]> stream = messageStreams.get(topic).get(0);
+		ConsumerIterator<byte[], byte[]> iterator = stream.iterator();
+		while (iterator.hasNext()) {
+			try {
+				String message = new String(iterator.next().message());
+				System.out.println("收到消息" + message);
+			} catch (Throwable e) {
+				System.out.println(e.getCause());
 			}
 		}
+
+	}
+
+	public void start() {
+		System.out.println("开始消费消息...");
+		Executors.newSingleThreadExecutor().execute(new Runnable() {
+
+			public void run() {
+				init();
+				while (true) {
+					try {
+						consume();
+					} catch (Throwable e) {
+						if (consumer != null) {
+							try {
+								consumer.shutdown();
+							} catch (Throwable e1) {
+								System.out.println("Turn off Kafka consumer error! " + e);
+							}
+						}
+					}
+				}
+			}
+		});
 	}
 }
--- src/main/java/kafkaTest/ProducerSample.java
+++ src/main/java/kafkaTest/ProducerSample.java
@@ -1,50 +1,78 @@
 package kafkaTest;
 
 import java.util.Properties;
+import java.util.concurrent.Executors;
 
-import org.apache.kafka.clients.producer.KafkaProducer;
-import org.apache.kafka.clients.producer.Producer;
-import org.apache.kafka.clients.producer.ProducerRecord;
+import kafka.javaapi.producer.Producer;
+import kafka.producer.KeyedMessage;
+import kafka.producer.ProducerConfig;
 
 public class ProducerSample {
-	public static void main(String[] args){
-//        long events = Long.parseLong(args[0]);
-//        Random rnd = new Random();
-// 
-//        Properties props = new Properties();
-//        props.put("metadata.broker.list", "broker1:9092,broker2:9092 ");
-//        props.put("serializer.class", "kafka.serializer.StringEncoder");
-//        props.put("partitioner.class", "example.producer.SimplePartitioner");
-//        props.put("request.required.acks", "1");
-// 
-//        ProducerConfig config = new ProducerConfig(props);
-// 
-//        Producer<String, String> producer = new Producer<String, String>(config);
-// 
-//        for (long nEvents = 0; nEvents < events; nEvents++) { 
-//               long runtime = new Date().getTime();  
-//               String ip = "192.168.2." + rnd.nextInt(255); 
-//               String msg = runtime + ",www.example.com," + ip; 
-//               KeyedMessage<String, String> data = new KeyedMessage<String, String>("page_visits", ip, msg);
-//               producer.send(data);
-//        }
-//        producer.close();
-        
-        
-        Properties props = new Properties();
-        props.put("bootstrap.servers", "localhost:9092");
-        props.put("acks", "all");
-        props.put("retries", 0);
-        props.put("batch.size", 16384);
-        props.put("linger.ms", 1);
-        props.put("buffer.memory", 33554432);
-        props.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
-        props.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+	private Producer<String, String> producer;
 
-        Producer<String, String> producer = new KafkaProducer<String, String>(props);
-        for(int i = 0; i < 100; i++)
-            producer.send(new ProducerRecord<String, String>("my-topic", Integer.toString(i), Integer.toString(i)));
+	public static void main(String[] args) {
+		new ProducerSample().start();
+	}
 
-        producer.close();
+	public void init() {
+		Properties props = new Properties();
+		/**
+		 * 用于自举（bootstrapping ），producer只是用它来获得元数据（topic, partition, replicas）
+		 * 实际用户发送消息的socket会根据返回的元数据来确定
+		 */
+		props.put("metadata.broker.list", "localhost:9092");
+		/**
+		 * 消息的序列化类 默认是 kafka.serializer.DefaultEncoder， 输入时 byte[] 返回是同样的字节数组
+		 */
+		props.put("serializer.class", "kafka.serializer.StringEncoder");
+		/**
+		 * producer发送消息后是否等待broker的ACK，默认是0 1 表示等待ACK，保证消息的可靠性
+		 */
+		props.put("request.required.acks", "1");
+		ProducerConfig config = new ProducerConfig(props);
+		// 泛型参数分别表示 The first is the type of the Partition key, the second the type of
+		// the message
+		producer = new Producer<String, String>(config);
+	}
+
+	public void produceMsg() {
+		// 构建发送的消息
+		long timestamp = System.currentTimeMillis();
+		String msg = "Msg" + timestamp;
+		String topic = "test"; // 确保有这个topic
+		System.out.println("发送消息" + msg);
+		String key = "Msg-Key" + timestamp;
+
+		/**
+		 * topic: 消息的主题 key：消息的key，同时也会作为partition的key message:发送的消息
+		 */
+		KeyedMessage<String, String> data = new KeyedMessage<String, String>(topic, key, msg);
+
+		producer.send(data);
+	}
+
+	public void start() {
+		System.out.println("开始发送消息 ...");
+		Executors.newSingleThreadExecutor().execute(new Runnable() {
+			public void run() {
+				init();
+				while (true) {
+					try {
+						produceMsg();
+						Thread.sleep(2000);
+					} catch (Throwable e) {
+						if (producer != null) {
+							try {
+								producer.close();
+							} catch (Throwable e1) {
+								System.out.println("Turn off Kafka producer error! " + e);
+							}
+						}
+					}
+
+				}
+
+			}
+		});
 	}
 }
